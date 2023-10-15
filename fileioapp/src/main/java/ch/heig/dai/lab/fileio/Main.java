8d9e02a7f4a226b0a3ddc012db2303f285ab3119
Patrick2ooo <p.maillard1522@gmail.com>
2023-10-15 10:20:09 +0200

    Implement Main for Patrick2ooo

--- fileioapp/src/main/java/ch/heig/dai/lab/fileio/Main.java
+++ fileioapp/src/main/java/ch/heig/dai/lab/fileio/Main.java
@@ -1,9 +1,11 @@
 package ch.heig.dai.lab.fileio;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.charset.Charset;
 
 // *** TODO: Change this to import your own package ***
-import ch.heig.dai.lab.fileio.jehrensb.*;
+import ch.heig.dai.lab.fileio.Patrick2ooo.*;
 
 public class Main {
     // *** TODO: Change this to your own name ***
@@ -31,15 +33,43 @@ public class Main {
         String folder = args[0];
         int wordsPerLine = Integer.parseInt(args[1]);
         System.out.println("Application started, reading folder " + folder + "...");
+        // mvn package
+        // java -jar target/fileioapp-1.0.jar /home/pmaillard/Desktop/jokes/ 5
         // TODO: implement the main method here
 
-        while (true) {
+        FileExplorer fileExplorer = new FileExplorer(folder);
+        EncodingSelector encodingSelector = new EncodingSelector();
+        FileReaderWriter fileReaderWriter = new FileReaderWriter();
+        Transformer transformer = new Transformer(newName, wordsPerLine);
+
+        File actualFile;
+
+        while ((actualFile = fileExplorer.getNewFile()) != null) {
             try {
                 // TODO: loop over all files
+                if(actualFile.getName().endsWith(".New")){
+                    continue;
+                }
+
+                Charset actualCharset = encodingSelector.getEncoding(actualFile);
+
+                String content = fileReaderWriter.readFile(actualFile, actualCharset);
+
+                content = transformer.replaceChuck(content);
+                content = transformer.capitalizeWords(content);
+                content = transformer.wrapAndNumberLines(content);
+
+                File outputFile = new File(actualFile.getPath() + ".New"); // probleme ici
+                if(!fileReaderWriter.writeFile(outputFile, content, actualCharset)){
+                    throw new IOException("file couldn't be written");
+                }
+
+                System.out.println("New File created");
 
             } catch (Exception e) {
                 System.out.println("Exception: " + e);
             }
         }
-    }
+        System.out.println("Process finished");
+    }    
 }
