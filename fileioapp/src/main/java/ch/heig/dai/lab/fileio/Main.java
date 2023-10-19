0c66870029a6a1135e0adbacadb9e97ffa8a1823
tomas-pavoni <tomas.pavoni@heig-vd.ch>
2023-10-19 12:54:09 +0200

    Main

--- fileioapp/src/main/java/ch/heig/dai/lab/fileio/Main.java
+++ fileioapp/src/main/java/ch/heig/dai/lab/fileio/Main.java
@@ -1,13 +1,13 @@
 package ch.heig.dai.lab.fileio;
 
 import java.io.File;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 
-// *** TODO: Change this to import your own package ***
-import ch.heig.dai.lab.fileio.jehrensb.*;
+import ch.heig.dai.lab.fileio.tomaspavoni.*;
 
 public class Main {
-    // *** TODO: Change this to your own name ***
-    private static final String newName = "Jean-Claude Van Damme";
+    private static final String newName = "Tomas Pavoni";
 
     /**
      * Main method to transform files in a folder.
@@ -31,11 +31,39 @@ public class Main {
         String folder = args[0];
         int wordsPerLine = Integer.parseInt(args[1]);
         System.out.println("Application started, reading folder " + folder + "...");
-        // TODO: implement the main method here
+
+        FileExplorer fileExplorer = new FileExplorer(folder);
+        EncodingSelector encodingSelector = new EncodingSelector();
+        FileReaderWriter fileReaderWriter = new FileReaderWriter();
+        Transformer transformer = new Transformer(newName, wordsPerLine);
+
 
         while (true) {
             try {
-                // TODO: loop over all files
+                File inputFile = fileExplorer.getNewFile();
+                if (inputFile == null) {
+                    break;
+                }
+                if (inputFile.getName().contains(".processed")) {
+                    continue;
+                };
+                Charset encoding = encodingSelector.getEncoding(inputFile);
+                if (encoding == null) {
+                    throw new RuntimeException(inputFile.getName() + " has no extension");
+                }
+                String txt = fileReaderWriter.readFile(inputFile, encoding);
+                if (txt == null) {
+                    throw new RuntimeException(inputFile + " is empty");
+                }
+                txt = transformer.replaceChuck(txt);
+                txt = transformer.capitalizeWords(txt);
+                txt = transformer.wrapAndNumberLines(txt);
+
+                String outputPath = inputFile.getPath() + ".processed";
+                File outputFile = new File(outputPath);
+                if(!fileReaderWriter.writeFile(outputFile, txt, StandardCharsets.UTF_8)) {
+                    throw new RuntimeException(inputFile.getName() + "can not be written in " + outputFile.getName());
+                }
 
             } catch (Exception e) {
                 System.out.println("Exception: " + e);
