d11acbd68d05beee02932930a8445189d8737d50
Juergen Ehrensberger <juergen.ehrensberger@gmail.com>
2023-11-22 10:55:56 +0100

    Correct main after merge from Calum-Quinn

--- fileioapp/src/main/java/ch/heig/dai/lab/fileio/Main.java
+++ fileioapp/src/main/java/ch/heig/dai/lab/fileio/Main.java
@@ -1,16 +1,13 @@
 package ch.heig.dai.lab.fileio;
 
 import java.io.File;
-import java.nio.charset.Charset;
-import java.nio.charset.StandardCharsets;
-import java.util.Objects;
 
 // *** TODO: Change this to import your own package ***
-import ch.heig.dai.lab.fileio.Calum_Quinn.*;
+import ch.heig.dai.lab.fileio.jehrensb.*;
 
 public class Main {
     // *** TODO: Change this to your own name ***
-    private static final String NEW_NAME = "Calum Quinn";
+    private static final String newName = "Jean-Claude Van Damme";
 
     /**
      * Main method to transform files in a folder.
@@ -33,46 +30,12 @@ public class Main {
         }
         String folder = args[0];
         int wordsPerLine = Integer.parseInt(args[1]);
-        System.out.println("Application started, reading folder " + folder);
+        System.out.println("Application started, reading folder " + folder + "...");
         // TODO: implement the main method here
 
-        FileExplorer fileExplorer = new FileExplorer(folder);
-        EncodingSelector encodingSelector = new EncodingSelector();
-        FileReaderWriter fileReaderWriter = new FileReaderWriter();
-        Transformer transformer = new Transformer(NEW_NAME, wordsPerLine);
-
-        File currentFile;
-
-        while ((currentFile = fileExplorer.getNewFile()) != null) {
+        while (true) {
             try {
                 // TODO: loop over all files
-
-                // Don't reprocess files
-                if (currentFile.getName().endsWith(".processed")) {
-                    continue;
-                }
-
-                System.out.println("Begin file processing " + currentFile.getName());
-
-                // Get charset
-                Charset currentCharset = encodingSelector.getEncoding(currentFile);
-                Objects.requireNonNull(currentCharset, "File extension unrecognised");
-
-                // Read file
-                String content = fileReaderWriter.readFile(currentFile, currentCharset);
-                Objects.requireNonNull(content, "Unable to read content");
-
-                // Transform content
-                content = transformer.replaceChuck(content);
-                content = transformer.capitalizeWords(content);
-                content = transformer.wrapAndNumberLines(content);
-
-                // Write result to file
-                File result = new File(currentFile.getAbsolutePath() + ".processed");
-                if (fileReaderWriter.writeFile(result, content, StandardCharsets.UTF_8)) {
-                    throw new RuntimeException("Could not write to file");
-                }
-                System.out.println("File processed");
 
             } catch (Exception e) {
                 System.out.println("Exception: " + e);
